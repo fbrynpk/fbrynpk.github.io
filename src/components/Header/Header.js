d7ea4d5ca0e5bca92d667c8913f608753155fe6e
Febryan Putra Kartika <febryanputra12@gmail.com>
2022-11-04 08:54:35 +0000

    update websites

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,30 @@
-import Link from 'next/link';
-import React from 'react';
-import * as FaIcons from 'react-icons/fa';
+import Link from "next/link";
+import React from "react";
+import * as FaIcons from "react-icons/fa";
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
+import {
+  Container,
+  Div1,
+  Div2,
+  Div3,
+  NavLink,
+  SocialIcons,
+  Span,
+} from "./HeaderStyles";
 
-const Header = () =>  (
+const Header = () => (
   <Container>
     <Div1>
       <Link href="/">
-        <a style={{display:'flex', alignItems:'center', color:'white', marginBottom: '20px'}}>
-          <FaIcons.FaBars size="2rem"/> <Span>Home</Span>
+        <a
+          style={{
+            display: "flex",
+            alignItems: "center",
+            color: "white",
+            marginBottom: "20px",
+          }}
+        >
+          <FaIcons.FaBars size="2rem" /> <Span>Home</Span>
         </a>
       </Link>
     </Div1>
@@ -32,13 +47,13 @@ const Header = () =>  (
     </Div2>
     <Div3>
       <SocialIcons href="https://github.com/fbrynpk">
-        <FaIcons.FaGithub size="4rem"/>
+        <FaIcons.FaGithub size="4rem" />
       </SocialIcons>
       <SocialIcons href="https://www.linkedin.com/in/febryan-putra-kartika-592934207/">
-        <FaIcons.FaLinkedin size="4rem"/>
+        <FaIcons.FaLinkedin size="4rem" />
       </SocialIcons>
-      <SocialIcons href="https://www.instagram.com/febryanpk/">
-        <FaIcons.FaInstagram size="4rem"/>
+      <SocialIcons href="https://www.instagram.com/fbrynpk/">
+        <FaIcons.FaInstagram size="4rem" />
       </SocialIcons>
     </Div3>
   </Container>
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,35 +1,33 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection, RightSection} from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
   <div>
     <Section row nopadding>
       <LeftSection>
         <SectionTitle main center>
-          Hello,<br/>
-          My name is<br/>
+          Hello,
+          <br />
+          My name is
+          <br />
           Febryan Putra Kartika
         </SectionTitle>
         <SectionText>
-          A third year student majoring in Electrical Engineering and Computer Science in National Taipei University of Technology
-          passionate about learning technology and developing program.
+          A third year student majoring in Electrical Engineering and Computer
+          Science in National Taipei University of Technology passionate about
+          learning technology and developing program.
         </SectionText>
         <form method="get" action="cv.pdf">
-          <Button type="submit">
-            Curriculum Vitae
-          </Button>
+          <Button type="submit">Curriculum Vitae</Button>
         </form>
       </LeftSection>
-      <RightSection>
-      <a href = "cv.pdf" download>
-        <Button type ="submit">
-          Click to Download!
-        </Button>
-      </a>
-      </RightSection>
     </Section>
   </div>
 );

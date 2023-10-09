8fd0cfe1b54ee039d455ada3063d1591b18c44d4
Febryan Putra Kartika <febryanputra12@gmail.com>
2023-10-09 18:31:17 +0700

    Improve UI for small devices

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -17,7 +17,7 @@ export const GridContainer = styled.section`
   @media ${(props) => props.theme.breakpoints.sm} {
     display: flex;
     flex-direction: column;
-    padding: 2rem;
+    padding: 2rem 2rem 10px;
     padding-bottom: 0;
   }
 `;
@@ -39,7 +39,6 @@ export const TitleContent = styled.div`
 
 export const HeaderThree = styled.h3`
   font-weight: 500;
-  letter-spacing: 2px;
   color: #9cc9e3;
   padding: 0.5rem 0;
   font-size: ${(props) => (props.title ? "3rem" : "2rem")};
@@ -71,7 +70,7 @@ export const CardInfo = styled.p`
   line-height: 24px;
   text-align: justify;
   @media ${(props) => props.theme.breakpoints.sm} {
-    padding: 0.3rem;
+    padding: 3rem;
   }
 `;
 

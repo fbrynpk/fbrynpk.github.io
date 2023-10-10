fb0c722871e85ccc24f57660fcc37e5422b05c02
Febryan Putra Kartika <febryanputra12@gmail.com>
2023-10-10 16:42:50 +0700

    Reduce gap between navbar and section

--- src/styles/GlobalComponents/index.js
+++ src/styles/GlobalComponents/index.js
@@ -10,7 +10,7 @@ export const Section = styled.section`
   position: relative;
   overflow: hidden;
   grid-template-columns: 1fr 1fr;
-  padding-top: 12%;
+  padding-top: 9%;
   padding-left: 10%;
 
   @media ${(props) => props.theme.breakpoints.lg} {

1477844164bd1e193da2caf8c3b55d06c6924a1d
Febryan Putra Kartika <febryanputra12@gmail.com>
2023-12-04 15:59:32 +0800

    Update Experiences

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { DiFirebase, DiReact, DiZend } from "react-icons/di";
+import {
+  FaPython,
+  FaBriefcaseMedical,
+  FaSpellCheck,
+  FaEye,
+} from "react-icons/fa";
 import {
   Section,
   SectionDivider,
   SectionText,
-  SectionTitle
+  SectionTitle,
 } from "../../styles/GlobalComponents";
 import {
   List,
   ListContainer,
   ListItem,
   ListParagraph,
-  ListTitle
+  ListTitle,
 } from "./TechnologiesStyles";
 
 const Technologies = () => (
@@ -26,9 +31,10 @@ const Technologies = () => (
     </SectionText>
     <List>
       <ListItem>
-        <DiFirebase size="3rem" />
         <ListContainer>
-          <ListTitle>Machine Learning</ListTitle>
+          <ListTitle>
+            <FaPython size="2rem" /> Machine Learning
+          </ListTitle>
           <ListParagraph>
             Familiar with Machine Learning algorithms (Linear and Logistic
             Regression, Decision Tree, Random Forest, Naive Bayes, SVM, KNN) and
@@ -37,24 +43,48 @@ const Technologies = () => (
         </ListContainer>
       </ListItem>
       <ListItem>
-        <DiReact size="3rem" />
         <ListContainer>
-          <ListTitle>Natural Language Processing</ListTitle>
+          <ListTitle>
+            <FaBriefcaseMedical size="2rem" />
+            &nbsp;Healthcare
+          </ListTitle>
           <ListParagraph>
-            Experienced with NLP theory such as Tokenizations, Vectorization,
-            Bag of Words model, Lemmatization and Stemming, markov models also
-            experimented with transformer models and how to fine-tune LLM.
+            Interested in how healthcare quality could be improved with the help
+            of innovative machine learning solutions and AI powered tools from
+            early detection, diagnosis, treatment, and monitoring of diseases.
           </ListParagraph>
         </ListContainer>
       </ListItem>
       <ListItem>
-        <DiZend size="3rem" />
         <ListContainer>
-          <ListTitle>Healthcare</ListTitle>
+          <ListTitle>
+            <FaSpellCheck size="2rem" />
+            &nbsp;Natural Language Processing
+          </ListTitle>
           <ListParagraph>
-            Interested in how healthcare quality could be improved with the help
-            of machine learning from early detection, diagnosis, treatment, and
-            monitoring of diseases.
+            Experienced with NLP theory such as Tokenizations, Vectorization,
+            Bag of Words model, Lemmatization and Stemming, markov models also
+            experimented with transformer models and how to fine-tune LLM such
+            as BERT, T5, etc. Currently understanding more about NLP and Speech
+            Technologies to facilitate more natural conversations between
+            machines and humans, ultimately bridging the gap, enabling seamless
+            and natural interactions between machines and users.
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <ListContainer>
+          <ListTitle>
+            <FaEye size="2rem" />
+            &nbsp;Computer Vision
+          </ListTitle>
+          <ListParagraph>
+            Proficient in CV, including image preprocessing, deep learning
+            architectures like CNNs, and implementing object detection and image
+            segmentation algorithms. Utilized state-of-the-art models such as
+            YOLO, and Vision Transformers (ViT) for various tasks. Fine-tuned
+            pre-trained models such as ResNet and VGG. Interested in learning
+            more about CV for Biomedical Image Analysis
           </ListParagraph>
         </ListContainer>
       </ListItem>
